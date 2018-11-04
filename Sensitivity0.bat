25db12c9db44dd78adbf6dbfcf836b55a5ca2462
MrMicko <mikko.kruunari@gmail.com>
2018-11-04 20:34:44 +0200

    fixed paths, raised hz

--- Sensitivity0.bat
+++ Sensitivity0.bat
@@ -1,10 +1,10 @@
 d:
-cd progs\GlovePIE045Free\omat
+cd progs\GlovePIE045Free\omat\GaRe
 REM EnablePedals.lnk
-start ..\piefree.exe -GameWASD_v5_b1.pie  /tray
-start ..\piefree.exe -GameMouse_HYDRA_V5_B1.pie  /tray
+start ..\..\piefree.exe -GameWASD_v5_b1.pie  /tray
+start ..\..\piefree.exe -GameMouse_HYDRA_V5_B1.pie  /tray
 timeout 2
-start ..\piefree.exe -Sensitivity0.pie
+start ..\..\piefree.exe -Sensitivity0.pie
 Choice /M "Disable pedals and kill Pies?"
 If Errorlevel 2 Goto No
 If Errorlevel 1 Goto Yes
