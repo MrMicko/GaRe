ac26c941ec3a3dac2a15b7c25a2d3af9146e2c5d
MrMicko <mikko.kruunari@gmail.com>
2019-01-20 09:36:37 +0200

    Update Sensitivity0.bat

--- Sensitivity0.bat
+++ Sensitivity0.bat
@@ -3,8 +3,8 @@ cd progs\GlovePIE045Free\omat\GaRe
 REM EnablePedals.lnk
 start ..\..\piefree.exe -GameWASD_v5_b1.pie  /tray
 start ..\..\piefree.exe -GameMouse_HYDRA_V5_B1.pie  /tray
-timeout 2
-start ..\..\piefree.exe -Sensitivity0.pie
+timeout 1
+start ..\..\piefree.exe -Sensitivity0.pie  /tray
 Choice /M "Disable pedals and kill Pies?"
 If Errorlevel 2 Goto No
 If Errorlevel 1 Goto Yes
