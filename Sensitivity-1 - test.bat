a0f8ca1e770d8922eb7ff386877c5544bd1bda8c
MrMicko <mikko.kruunari@gmail.com>
2020-09-11 21:46:51 +0300

    debug more, most like revert

---
+++ Sensitivity-1 - test.bat
@@ -0,0 +1,19 @@
+d:
+cd progs\GlovePIE045Free\omat\GaRe
+REM start "" /High ..\..\piefree.exe -GameWASD_v5_b1.pie  /tray
+start "" /High ..\..\piefree.exe -GameMouse_HYDRA_V5_B1.pie
+timeout 1
+start ..\..\piefree.exe -Sensitivity-1.pie
+Choice /M "Kill Pies?"
+If Errorlevel 2 Goto No
+If Errorlevel 1 Goto Yes
+Goto End
+:No
+Goto End
+:Yes
+REM DisablePedals.lnk
+KillPie.lnk
+Goto End
+:End
+
+exit
